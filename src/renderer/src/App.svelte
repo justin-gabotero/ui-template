ff18cd503c34e5cfbe6b4b555e0c3c6103c6b329
Justin Gabotero <shinjozyro@gmail.com>
2024-10-28 03:26:41 +0800

    Add new files and configurations

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  // ipc handle
+  const ipcHandle = () => {
+    window.electron.ipcRenderer.send('ping')
+  }
+</script>
+
+<div class="action">
+  ipc button
+  <button on:click={ipcHandle}>ping</button>
+</div>
